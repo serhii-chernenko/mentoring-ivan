55d46677268ce6d03547512151b78877cee8301e
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-12-04 21:05:42 +0300

    remove: string-ends-with kata

--- katas/7kyu/reverse-words.js
+++ katas/7kyu/reverse-words.js
@@ -1,6 +1,6 @@
 function reverseWords(str) {
-    const words = str.split(' ');
+    const words = str.split(/\s/);
     const wordsReverse = words.map(word => word.split('').reverse().join(''));
 
-    return wordsReverse.join(' ');
+    return wordsReverse.join(/\s/);
 }
--- katas/7kyu/string-ends-with.js
+++
@@ -1,8 +0,0 @@
-function solution(str, ending) {
-    if (!ending) return true;
-
-    const endingLength = ending.length;
-    const strEnd = str.slice(-endingLength);
-
-    return strEnd === ending;
-}
