59bbff77769a6155e63c7b84df25be11c3592c23
Ivan Kondzerau <konderev@aheadworks.com>
2024-12-04 22:49:51 +0300

    Remove: reverse-words.js

--- katas/7kyu/reverse-words.js
+++
@@ -1,6 +0,0 @@
-function reverseWords(str) {
-    const words = str.split(/\s/);
-    const wordsReverse = words.map(word => word.split('').reverse().join(''));
-
-    return wordsReverse.join(/\s/);
-}
